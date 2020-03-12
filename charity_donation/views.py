cae66065b7d910c5b3bd867cb42728bf7f8ecc3f
Wojciech Mzyk <W_Mzyk@interia.pl>
2020-03-12 23:26:18 +0100

    Wykonana rejestracja

--- charity_donation/views.py
+++ charity_donation/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 from django.views import View
 
 from charity_donation.models import Donation, Institution
@@ -33,3 +35,12 @@ class Register(View):
 
     def get(self, request):
         return render(request, 'register.html')
+
+    def post(self, request):
+        name = request.POST.get('name')
+        surname = request.POST.get('surname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        o = User.objects.create_user(username=name, email=email, password=password)
+        return redirect(reverse_lazy('login'))
